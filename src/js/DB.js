ee959c55d93cb261d86ddb615c3d322748d0579d
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-09-05 19:24:06 +0200

    Create DataBase class

--- src/js/DB.js
+++ src/js/DB.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const Ticket = require('./Ticket');
+const TicketFull = require('./TicketFull');
 
 class DataBase {
   constructor() {
@@ -7,20 +8,26 @@ class DataBase {
   }
 
   getAllTickets() {
-    return this.data.tickets;
+    return this.parse().ticket;
   }
 
   getTicketById(id) {
-    return this.data.ticketsFull.find((ticket) => ticket.id === Number(id));
+    return this.parse.ticketsFull.find((ticket) => ticket.id === Number(id));
   }
 
   postTicket(ticketData) {
-    const ticket = new Ticket(ticketData.name, ticketData.status);
+    const data = JSON.parse(ticketData);
+    const ticket = new Ticket(data.name, data.status);
+    const ticketFull = new TicketFull(data.name, data.description, data.status);
+
     ticket.id = this.generateId();
+    ticketFull.id = ticket.id;
+    ticketFull.created = ticket.created;
 
     fs.readFile(this.DBUrl, 'utf-8', () => {
       const dataObject = this.parse();
       dataObject.tickets.push(ticket);
+      dataObject.ticketsFull.push(ticketFull);
 
       fs.writeFile(this.DBUrl, JSON.stringify(dataObject), 'utf-8', () => {
         console.log(`ticket ${ticket.name} add to database`);
